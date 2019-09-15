31cd5678b2b04a239cfae637afaf53303335a7ba
chenjinxin <chenjinxin666@gmail.com>
2019-09-15 20:21:48 +0800

    登录注册，更多

--- README.md
+++ README.md
@@ -4,3 +4,6 @@ Windows下python 版本选择：
     2.python.exe重命名->python2 使用python2.x;python3 使用python3.x
 mysql数据库：8.0.11:
     pip install -i https://pypi.tuna.tsinghua.edu.cn/simple --upgrade sqlalchemy --ignore-installed
+    __init__.py 添加
+        import pymysql
+        pymysql.install_as_MySQLdb()
--- project/__init__.py
+++ project/__init__.py
@@ -2,12 +2,18 @@
 
 from flask import Flask
 import pymysql
+
 pymysql.install_as_MySQLdb()
+
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.jinja_env.add_extension('jinja2.ext.loopcontrols')
 app.config.from_pyfile('app.conf')
+app.secret_key = 'chenjinxin'
 db = SQLAlchemy(app)
+login_manager = LoginManager(app)
+login_manager.login_view = '/regloginpage/'
 
 from project import views, models
